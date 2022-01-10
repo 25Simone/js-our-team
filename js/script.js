b2048805954d6655b917069de148a3dc6190393b
25Simone <dantsimone111@gmail.com>
2022-01-10 16:15:03 +0100

    impostazione bonus

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
+
 const team = [
     {
-      name: 'Wayne Barnett',
-      role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
 ];
 
@@ -49,15 +50,35 @@ for(let i = 1; i < team.length; i++) {
 
 // or
 // for(let i = 1; i < team.length; i++){
-//     let teamCard = document.createElement('div');
-//     teamCard.className = 'team-card';
-//     let cardImg = document.createElement('div');
-//     cardImg.className = 'card-image';
-//     cardImg.innerHTML += `<img src="img/${team[i].image}" alt="${team[i].name}"/>`;
-//     let cardText = document.createElement('div')
-//     cardText.className = 'card-text';
-//     cardText.innerHTML += `<h3>${team[i].name}</h3>`;
-//     cardText.innerHTML += `<p>${team[i].role}</p>`;
-//     teamCard.append(cardImg, cardText);
-//     outputHtml.append(teamCard);
-// }
+    //     let teamCard = document.createElement('div');
+    //     teamCard.className = 'team-card';
+    //     let cardImg = document.createElement('div');
+    //     cardImg.className = 'card-image';
+    //     cardImg.innerHTML += `<img src="img/${team[i].image}" alt="${team[i].name}"/>`;
+    //     let cardText = document.createElement('div')
+    //     cardText.className = 'card-text';
+    //     cardText.innerHTML += `<h3>${team[i].name}</h3>`;
+    //     cardText.innerHTML += `<p>${team[i].role}</p>`;
+    //     teamCard.append(cardImg, cardText);
+    //     outputHtml.append(teamCard);
+    // }
+    
+    // BONUS
+    // definisco la variabile del bottone
+    const addMemberButton = document.getElementById('addMemberButton');
+    
+    // definisco l'evento al click del bottone
+    addMemberButton.addEventListener('click', () => {
+        let newName = document.getElementById('name').value;
+        let newRole = document.getElementById('role').value;
+        let newImg = document.getElementById('image').value;
+        const object = {
+            name: newName,
+            role: newRole,
+            image: newImg,
+        }
+        team.push(object);
+    })
+    
+    
+    
